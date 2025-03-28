4c915c892d572082cbccd768749d313a1d0fed10
Mamidi Manohar <manoharmamidi2001@gmail.com>
2025-03-28 15:53:57 +0530

    path

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
         <Routes>
-            <Route path='/' element={<WeatherCard/>}/>
+            <Route path='/weather' element={<WeatherCard/>}/>
         </Routes>
     </div>
   )
